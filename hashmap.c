d581a6f3edd2faba2ebeda98db672582b374e92c
repl.it user <replituser@example.com>
2020-09-25 00:52:41 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -90,8 +90,8 @@ void eraseMap(HashMap * map,  char * key) {
 void * searchMap(HashMap * map,  char * key) {   
   long cap = hash(key, map -> capacity);
   for (; cap < map->capacity ;cap++){
-    if  (map -> buckets[cap] == NULL){
-      return NULL;
+    if (map -> buckets[cap] == NULL){
+      break;
     }
     if (is_equal(key,map -> buckets[cap] -> key)){
       map -> current = cap;
